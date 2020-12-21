fe5f34c71033ab6f7a61d48b234e7de298cd0bca
swrko <inviiik@gmail.com>
2020-12-21 02:22:20 +0100

    normalize input function + code cleaning

--- neuro.py
+++ neuro.py
@@ -5,31 +5,28 @@ from random import random
 
 # Calculate neuron activation for an input
 def activate(weights, inputs):
-    activation = weights[-1]
-    for i in range(len(weights) - 1):
+    activation = 0
+    # activation = weights[-1] # pre bias ak by bol posledny
+    for i in range(len(weights)):
         activation += weights[i] * inputs[i]
     return activation
-
-
-# Transfer neuron activation
-def transfer(activation):
-    return 1.0 / (1.0 + exp(-activation))
 
 
 # Forward propagate input to a network output
 def forward_propagation(network, row):
     inputs = row
+    new_inputs = []
     for layer in network:
-        new_inputs = []
         for neuron in layer:
             activation = activate(neuron['weights'], inputs)
-            neuron['output'] = transfer(activation)
+            neuron['output'] = 1.0 / (1.0 + exp(-activation))
             new_inputs.append(neuron['output'])
+
         inputs = new_inputs
     return inputs
 
-def main():
 
+def main():
     # seed(1)
 
     # test forward propagation
@@ -44,10 +41,11 @@ def main():
 
     for layer in network:
         print(layer)
-
-
-
-
+        for neuron in layer:
+            print(neuron)
+            for weights in neuron['weights']:
+                print(weights)
+    # print(normalize_inputs([0, 250, 400]))
 
 
 # Initialize a network
@@ -59,6 +57,16 @@ def init_network(n_inputs, n_hidden, n_outputs):
     output_layer = [{'weights': [random() for i in range(n_hidden)]} for i in range(n_outputs)]
     network.append(output_layer)
     return network
+
+
+def normalize_inputs(inputs):
+    min = 0
+    max = 400
+    new_inputs = []
+    for input in inputs:
+        new_input = (input - min) / (max - min)
+        new_inputs.append(new_input)
+    return new_inputs
 
 
 if __name__ == '__main__':
