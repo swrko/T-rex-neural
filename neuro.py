680110953a587a38ead3a6ae81b44c447f0ddf90
swrko <inviiik@gmail.com>
2020-12-25 14:00:26 +0100

    formating code

--- neuro.py
+++ neuro.py
@@ -11,10 +11,9 @@ def activate(weights, inputs):
         activation += weights[i] * inputs[i]
     return activation
 
-
 # Forward propagate input to a network output
-def forward_propagation(network, row):
-    inputs = row
+def forwardPropagation(network, norm_inputs):
+    inputs = norm_inputs
     new_inputs = []
     for layer in network:
         for neuron in layer:
@@ -25,6 +24,14 @@ def forward_propagation(network, row):
         inputs = new_inputs
     return inputs
 
+def normalizeInputs(inputs):
+    min = 0
+    max = 400
+    new_inputs = []
+    for input in inputs:
+        new_input = (input - min) / (max - min)
+        new_inputs.append(new_input)
+    return new_inputs
 
 def main():
     # seed(1)
@@ -33,7 +40,7 @@ def main():
     # network = [[{'weights': [0.13436424411240122, 0.8474337369372327, 0.763774618976614]}],
     #            [{'weights': [0.2550690257394217, 0.49543508709194095]},
     #             {'weights': [0.4494910647887381, 0.651592972722763]}]]
-    network = init_network(2, 5, 2)
+    network = initialiseNetwork(2, 5, 2)
 
     # row = [1, 0, None]
     # output = forward_propagation(network)
@@ -45,11 +52,11 @@ def main():
             print(neuron)
             for weights in neuron['weights']:
                 print(weights)
-    # print(normalize_inputs([0, 250, 400]))
+    # print(normalizeInputs([0, 250, 400]))
 
 
 # Initialize a network
-def init_network(n_inputs, n_hidden, n_outputs):
+def initialiseNetwork(n_inputs, n_hidden, n_outputs):
     network = list()
     # pre bias treba dat do vnutorneho foru +1 vahu
     hidden_layer = [{'weights': [random() for i in range(n_inputs)]} for i in range(n_hidden)]
@@ -58,15 +65,6 @@ def init_network(n_inputs, n_hidden, n_outputs):
     network.append(output_layer)
     return network
 
-
-def normalize_inputs(inputs):
-    min = 0
-    max = 400
-    new_inputs = []
-    for input in inputs:
-        new_input = (input - min) / (max - min)
-        new_inputs.append(new_input)
-    return new_inputs
 
 
 if __name__ == '__main__':
