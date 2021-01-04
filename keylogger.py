8ac755a3910d395510618c12fcea10cbe512783f
swrko <inviiik@gmail.com>
2021-01-04 16:34:02 +0100

    put keylogger in nonblocking mode

--- keylogger.py
+++ keylogger.py
@@ -1,18 +1,30 @@
 from pynput.keyboard import Key, Listener
+from pynput import keyboard
+from time import sleep
+
+
+def on_press(key):
+    if key == Key.up or key == Key.down:
+        print('{0} pressed'.format(
+            key))
+
+    if str(key) == "'q'":
+        return False
 
 
 def main():
-    def on_press(key):
-        if key == Key.up or key == Key.down:
-            print('{0} pressed'.format(
-                key))
+    # by documentation: runs as thread #non-blocking fashion
+    listener = keyboard.Listener(
+        on_press=on_press)
+    listener.start()
 
-        if str(key) == "'q'":
-            return False
+    while True:
+        sleep(1)
+        print("alive! ")
 
     # Listener
-    with Listener(on_press=on_press) as listener:
-        listener.join()
+    # with Listener(on_press=on_press) as listener:
+    #     listener.join()
 
 
 if __name__ == '__main__':
